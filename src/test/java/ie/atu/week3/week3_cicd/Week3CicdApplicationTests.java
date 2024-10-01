4967fbc0fa592f040b80d2bb518dd1a2e113048c
G00419108@atu.ie <G00419108@atu.ie>
2024-10-01 10:18:23 +0100

    New project

---
+++ src/test/java/ie/atu/week3/week3_cicd/Week3CicdApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week3.week3_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week3CicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
