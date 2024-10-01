84a4e3b7e96690902ce6ca0c7c9b3dda45995594
G00419108@atu.ie <G00419108@atu.ie>
2024-10-01 10:48:00 +0100

    Added message or details search in person

--- src/main/java/ie/atu/week3/week3_cicd/week2.java
+++ src/main/java/ie/atu/week3/week3_cicd/week2.java
@@ -2,9 +2,11 @@ package ie.atu.week3.week3_cicd;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/person")
 public class week2 {
     @GetMapping("/hello")
     public String message(){
@@ -15,4 +17,17 @@ public class week2 {
     {
         return "Your name is "+ name;
     }
+
+    @GetMapping("/details")
+    public String getDetails()
+    {
+        return "some message";
+    }
+
+    @GetMapping("/message")
+    public String getMessage(){
+        return "the message";
+
+    }
+
 }
